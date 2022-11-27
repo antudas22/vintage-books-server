779f0f34bd76fcd321d6215a23a99d0ed983c77c
antudas22 <20antu20@gmail.com>
2022-11-28 00:27:04 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lmouiy1.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+app.get('/', async(req, res) => {
+    res.send('Vintage Books server is running...');
+})
+
+app.listen(port, () => console.log(`Doctors portal running on ${port}`))
