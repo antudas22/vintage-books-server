1a6a3f44d72c80ffe5cea5e0d089a58c0a943d40
antudas22 <20antu20@gmail.com>
2022-11-28 00:48:08 +0600

    send data mongodb to client side

--- index.js
+++ index.js
@@ -14,7 +14,21 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lmouiy1.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run(){
+    try{
+        const productsCollection = client.db('vintageBooks').collection('products');
 
+        app.get('/products', async(req, res) =>{
+            const query = {};
+            const products = await productsCollection.find(query).toArray();
+            res.send(products);
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(console.log);
 
 app.get('/', async(req, res) => {
     res.send('Vintage Books server is running...');
